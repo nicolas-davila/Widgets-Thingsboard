e14dd19579eb24b35a5c6d023e9e503de1483288
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2023-08-03 15:42:53 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+devicesList = [
+    { id: 1, name: 'Dispositivo 1', type: 'sensor' },
+    { id: 2, name: 'Dispositivo 2', type: 'atuador' },
+    { id: 3, name: 'Dispositivo 3', type: 'sensor' },
+    // Mais dispositivos...
+];
